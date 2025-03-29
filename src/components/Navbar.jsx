e6b7837c9c504b198644915e67386849a3d7f12f
GonzaEspisle <128007689+GonzaEspisle@users.noreply.github.com>
2025-03-29 19:32:31 -0300

    Css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
+
 import { Link } from 'react-router-dom';
 
 function NavBar() {
     return (
         <nav>
-            <h1>Tienda Brightway</h1>
-            <Link to='/'>Inicio</Link>
-            <Link to='/category/electronica'>Electrónica</Link>
-            <Link to='/category/ropa'>Ropa</Link>
-            <Link to='/category/hogar'>Hogar</Link>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+                <img src='' alt='Logo' style={{ height: '40px', marginRight: '10px' }} />
+                <h1 style={{ margin: 0 }}>Tienda Brightway</h1>
+            </div>
+            <div>
+                <Link to='/'>Inicio</Link>
+                <Link to='/category/electronica'>Electrónica</Link>
+                <Link to='/category/ropa'>Ropa</Link>
+                <Link to='/category/hogar'>Hogar</Link>
+            </div>
         </nav>
     );
 }
