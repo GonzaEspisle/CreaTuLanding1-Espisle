1d54453995dc20846dbad38df96240e1e23fd106
GonzaEspisle <128007689+GonzaEspisle@users.noreply.github.com>
2025-04-26 02:10:47 -0300

    Comienzo de configuracion de Firebase

---
+++ src/services/firebaseconfig.js
@@ -0,0 +1,16 @@
+// firebase/firebaseConfig.js
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBMv16iYoK98qZy07WTtiA_QcbPtDE_q3g",
+    authDomain: "tienda-brightway.firebaseapp.com",
+    projectId: "tienda-brightway",
+    storageBucket: "tienda-brightway.appspot.com",
+    messagingSenderId: "191732010619",
+    appId: "1:191732010619:web:d9e489e0bc15caa43bdcde"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
