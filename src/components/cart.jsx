b1ddb1c7c02c376890c4d671aca828f47edfdc72
GonzaEspisle <128007689+GonzaEspisle@users.noreply.github.com>
2025-05-16 01:38:45 -0300

    Funcionalidad en ver mas y en agregar al carrito

--- src/components/cart.jsx
+++ src/components/cart.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { CartContext } from "./context/CartContext";
+import { CartContext } from "./context/cartcontext";
 import { Link } from "react-router-dom";
 import './Cart.css';
 
